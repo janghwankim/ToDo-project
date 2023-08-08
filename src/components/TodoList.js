eb75f56e83219a24ef8cbd62c48f8e77c9d7b2b7
janghwankim <hwan525@naver.com>
2023-08-08 23:53:48 +0900

    a3

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,7 +6,7 @@ import './TodoList.scss';
 const TodoList =({ todos, onRemove, onToggle }) => {
     const rowRenderer = useCallback(
         ({ index, key, style }) => {
-            const todo = todo[index];
+            const todo = todos[index];
             return(
                 <TodoListItem 
                 todo={todo} 
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -9,12 +9,15 @@ import './TodoListItem.scss';
 
 const TodoListItem = ({ todo, onRemove, onToggle, style}) => {
     const {id, text , checked} = todo;
+    
     return (
         <div className="TodoListItem-virtualized" style={style}>
         <div className="TodoListItem">
-            <div className={cn('checkbox', {checked})} onClick={()=>onToggle(id)}>
-                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-                <div className="text">{text}</div>
+          <div className={cn('checkbox', {checked})} 
+            onClick={()=>onToggle(id)}
+          >
+            {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+            <div className="text">{text}</div>
             </div>
             <div className="remove" onClick={() => onRemove(id)}>
                 <MdRemoveCircleOutline />
@@ -24,4 +27,7 @@ const TodoListItem = ({ todo, onRemove, onToggle, style}) => {
     );
 };
 
-export default React.memo(TodoListItem);
+export default React.memo(
+    TodoListItem,
+    (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+    );
